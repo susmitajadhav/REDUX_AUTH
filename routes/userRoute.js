5c515b600fdf6666ef341fbfb1b80c8bec5c2401
susmitajadhav <45027764+susmitajadhav@users.noreply.github.com>
2024-07-10 15:33:18 +0530

    Add files via upload

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const route = require('express').Router()
 
-const {register,login,getUser} = require('../controller/zz')
+const {register,login,getUser} = require('../controller/userController')
 
 route.post ('/',register)
 route.post('/login',login)
